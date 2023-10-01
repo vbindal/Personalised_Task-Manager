63d8533930ddf01de6a5d7e464cd1381c607188d
Vishal Bindal <vbindal027@gmail.com>
2023-10-01 13:05:34 +0530

    changes

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-<<<<<<< HEAD
     baseURL: 'http://localhost:3000' 
-=======
-    baseURL: 'https://personalised-task-manager-api.vercel.app/' 
->>>>>>> be776c429b8f9836d53dd8f9e63a19ad7742ff5f
 });
