ec3f351ae03a837cf300937f6e8806a715023621
Vishal Bindal <vbindal027@gmail.com>
2023-10-01 13:02:46 +0530

    changes

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,10 +1,5 @@
 import axios from 'axios'
 
-export const localAxios = axios.create({
-    baseURL: 'http://localhost:3000',
-  });
-  
-  export const remoteAxios = axios.create({
-    baseURL: 'https://personalised-task-manager-api.vercel.app/',
-  });
-  
+export default axios.create({
+    baseURL: 'http://localhost:3000' 
+});
