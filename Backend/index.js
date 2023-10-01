b7041f68187c936ec722d4ebcab1541771dc6d6a
Vishal Bindal <vbindal027@gmail.com>
2023-10-01 12:49:51 +0530

    add backend remote url

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,8 @@ const User = require('./models/user')
 
 const port = process.env.PORT || 3000
 const corsOptions = {
-  origin:['http://localhost:3001','http://localhost:3000'], 
+  origin:['http://localhost:3001','http://localhost:3000',
+  'https://personalised-task-manager-oyby.vercel.app/'], 
   methods : ["POST","GET"],
   credentials: true, 
 };
--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
-export default axios.create({
-    baseURL: 'http://localhost:3000' 
-});
+export const localAxios = axios.create({
+    baseURL: 'http://localhost:3000',
+  });
+  
+  export const remoteAxios = axios.create({
+    baseURL: 'https://personalised-task-manager-api.vercel.app/',
+  });
+  
